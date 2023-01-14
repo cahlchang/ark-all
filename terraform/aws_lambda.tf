3bf149433e79da59fcd71e3c6d0a6b87957ea112
morin_river <kahlua.dane@gmail.com>
2023-01-14 19:55:54 +0900

    discord bot lambda's terraform settings.

---
+++ terraform/aws_lambda.tf
@@ -0,0 +1,19 @@
+resource "aws_lambda_function" "ark-server-bot" {
+  filename      = "files/handler.py.zip"
+  function_name = "ark-server-bot"
+  role          = aws_iam_role.whl-basic-lambda.arn
+  handler       = "handler.run"
+
+  source_code_hash = filebase64sha256("files/handler.py.zip")
+
+  runtime = "python3.9"
+
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes = [
+      # last_modified,
+      # source_code_hash,
+      environment
+    ]
+  }
+}
---
+++ terraform/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  managed_by = "terraform"
+  repository = "cahlchang/ark-all-terraform-state"
+  aws_region = "ap-northeast-1"
+}
